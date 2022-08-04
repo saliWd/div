58148ca14bad506e86caa47baea640c532410a5c
saliWd <57621907+saliWd@users.noreply.github.com>
2022-08-04 17:04:35 +0200

    first trials to switch to power shell...

--- pico/widaktiv/prepareNmake.bat
+++ pico/widaktiv/prepareNmake.bat
@@ -4,12 +4,14 @@ setlocal
 if not exist "build/" mkdir build
 
 :: Holzhammermethode, delete some dirs
+:: does not work at the moment...
 if exist "build/CMakeFiles" del /S /Q "build/CMakeFiles" >nul 2>&1
 if exist "build/usb" del /S /Q "build/usb" >nul 2>&1
 if exist "build/st7789" del /S /Q "build/st7789" >nul 2>&1
 if exist "build/libraries" del /S /Q "build/libraries" >nul 2>&1
+:: do not change the pico-sdk / elf2uf2 / pioasm directories (shouldn't change that often and they are big)
 
-
+:: this one is working
 cd build
 :: source one directory up, build in this directory
 cmake -G "NMake Makefiles" -S .. -B .
