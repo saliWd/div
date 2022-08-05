2f3f1375ff0b23f58dec20ab93ae8b7c2de8a6fe
saliWd <57621907+saliWd@users.noreply.github.com>
2022-08-05 14:17:10 +0200

    works now

--- pico/widaktiv/prepareNmake.py
+++ pico/widaktiv/prepareNmake.py
@@ -2,6 +2,7 @@
 
 import os
 from pathlib import Path
+import subprocess
 
 def rmdir(directory):
     directory = Path(directory)
@@ -13,42 +14,41 @@ def rmdir(directory):
     directory.rmdir()
 
 current_dir = os.getcwd()
-build_dir = current_dir + "\\build" # escape the backslash
+build_dir = current_dir + '\\build' # escape the backslash
 
 build_dir_exist = os.path.exists(build_dir)
 
 if (build_dir_exist) :
-  cmakefiles_dir = build_dir + "\\CMakeFiles"
-  cmakefiles_dir_exist  = os.path.exists(cmakefiles_dir)
-  if (cmakefiles_dir_exist) :
-    # rmdir(Path(cmakefiles_dir))
-    print("need to delete the build, cmake directory")  
+  cmakefiles_dir = build_dir + '\\CMakeFiles'
+  usb_dir = build_dir + '\\usb'
+  st7789_dir = build_dir + '\\st7789'
+  libraries_dir = build_dir + '\\libraries'
   
+  # TODO: move into a loop over a list
+  if (os.path.exists(cmakefiles_dir)) :
+    rmdir(Path(cmakefiles_dir))
+    print('did delete the build, cmake directory')  
+  if (os.path.exists(usb_dir)) :
+    rmdir(Path(usb_dir))
+    print('did delete the build, usb directory')  
+  if (os.path.exists(st7789_dir)) :
+    rmdir(Path(st7789_dir))
+    print('did delete the build, st7789 directory')  
+  if (os.path.exists(libraries_dir)) :
+    rmdir(Path(libraries_dir))
+    print('did delete the build, libraries directory')   
+  ## do not change the pico-sdk / elf2uf2 / pioasm directories (shouldn't change that often and they are big)
   
-else : # build directory does not exist
+else : # build directory does not exist. create it
   os.mkdir(build)
 
+# now I am sure the build directory exists and the (user code) outputs are deleted
+os.chdir('build')
+p0 = subprocess.run('cmake -G "NMake Makefiles" -S .. -B .')
+os.chdir('..\\')
+
+# doesn't work at the moment. Don't know why though...
+# p1 = subprocess.run('dir build') # TODO: could use python functionality for that
+
+print('Done. Now use nmake (in build directory) with the Developer command console for VS2022')
 exit()
-
-
-## if not exist "build/" mkdir build
-## 
-## :: Holzhammermethode, delete some dirs
-## :: does not work at the moment...
-## if exist "build/CMakeFiles" del /S /Q "build/CMakeFiles" >nul 2>&1
-## if exist "build/usb" del /S /Q "build/usb" >nul 2>&1
-## if exist "build/st7789" del /S /Q "build/st7789" >nul 2>&1
-## if exist "build/libraries" del /S /Q "build/libraries" >nul 2>&1
-## :: do not change the pico-sdk / elf2uf2 / pioasm directories (shouldn't change that often and they are big)
-## 
-## :: this one is working
-## cd build
-## :: source one directory up, build in this directory
-## cmake -G "NMake Makefiles" -S .. -B .
-## cd ../
-## dir build
-## 
-## :: now use nmake (in build directory) with the Developer command console for VS2022
-## 
-## endlocal
-## @echo on
