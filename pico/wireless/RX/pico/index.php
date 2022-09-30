a7e2c19c6907557f42e3a0000f0aa48744ddc573
saliWd <57621907+saliWd@users.noreply.github.com>
2022-09-30 14:54:41 +0200

    small updates on status page

--- pico/wireless/RX/pico/index.php
+++ pico/wireless/RX/pico/index.php
@@ -4,7 +4,7 @@ $dbConn = initialize();
 
 echo '<!DOCTYPE html><html><head>
 <meta charset="utf-8" />
-<title>pico_w counter status auto refresh</title>
+<title>pico_w Empfänger</title>
 <meta name="description" content="a page displaying the value of the pico_w counter" />  
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="refresh" content="10; url=https://widmedia.ch/pico/">
@@ -14,13 +14,19 @@ echo '<!DOCTYPE html><html><head>
 </head><body>
 <div class="section noBottom">
 <div class="container">
-<h3>pico wireless receiver</h3>
+<h3>pico wireless: Empfänger</h3>
 <p>&nbsp;</p>';
 
 $doSafe = safeIntFromExt('GET', 'do', 2); // this is an integer (range 1 to 99) or non-existing
 if ($doSafe === 0) { // entry point of this site
   // select all entries
   $result = $dbConn->query('SELECT `id`, `device`, `value0`, `date` FROM `pico_w` WHERE 1 ORDER BY `id`');
+  $rowCnt = $result->num_rows;
+  
+  echo '<div class="row">
+          <div class="six columns">Insgesamt '.$rowCnt.' Einträge</div>
+          <div class="six columns"><div class="button"><a href="index.php?do=1">alle Einträge löschen</a></div></div>
+        </div>';
   while ($row = $result->fetch_assoc()) {
     $id = (int)$row['id'];  
     $value0 = (int)$row['value0'];  
@@ -32,15 +38,11 @@ if ($doSafe === 0) { // entry point of this site
             <div class="three columns">update: '.$row['date'].'</div>
           </div>';
   } // while
-  echo '<div class="row">
-            <div class="six columns"><div class="button"><a href="index.php?do=1">clear all entries</a></div></div>
-            <div class="six columns">&nbsp;</div>
-          </div>';
-  echo '<div class="row twelve columns">...refreshing every 10 seconds...</div>';
-} elseif ($doSafe === 1) { // delete all entries, then go back to default page  
-  $result = $dbConn->query('DELETE FROM `pico_w` WHERE 1'); // `device` = "home"); // MAYBE: want to delete only some things  
+  echo '<div class="row twelve columns">...diese Seite wird alle 10 Sekunden neu geladen...</div>';
+} elseif ($doSafe === 1) { // delete all entries, then go back to default page
+  $result = $dbConn->query('DELETE FROM `pico_w` WHERE 1'); // `device` = "home"); // MAYBE: want to delete only some things
   if ($result) {
-    echo '<div class="row twelve columns">...did delete all entries...</div>';
+    echo '<div class="row twelve columns">...alle Einträge gelöscht. <a href="index.php">zurück</a>...</div>';
   } else {
     echo '<div class="row twelve columns">...something went wrong when deleting all entries...</div>';
   }
