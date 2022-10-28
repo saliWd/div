35197115b9988ae5e8eeea60875ac5756b1546a1
saliWd <57621907+saliWd@users.noreply.github.com>
2022-10-28 16:05:41 +0200

    range selection working nicely

--- pico/wireless/RX/wmeter/index.php
+++ pico/wireless/RX/wmeter/index.php
@@ -37,36 +37,50 @@ function getTimeRange():int {
 
 $doSafe = safeIntFromExt('GET', 'do', 2); // this is an integer (range 1 to 99) or non-existing
 // do = 0: entry point, display graph and stuff
-// do = 1: delete all entries in DB
-// do = 2: 
+// do = 1: delete all entries in DB. TODO: different approach
 $autoreload = safeIntFromExt('GET', 'autoreload', 1);
 $timeSelected = getTimeRange();
 $enableAutoReload = ($autoreload === 1);
+$device = 'austr10'; // TODO: device as variable
+
 if ($doSafe === 0) { // entry point of this site
-  printBeginOfPage_index($enableAutoReload); // refreshes the page every x seconds
-  $QUERY_LIMIT = 5000; // TODO: check js-performance for a meaningful value
-  $GRAPH_LIMIT = 3; // does not make sense to display a graph otherwise
+  $resultCnt = $dbConn->query('SELECT COUNT(*) as `total` FROM `wmeter` WHERE `device` = "'.$device.'" LIMIT 1;'); // guaranteed to return one row
+  $resultFreshest = $dbConn->query('SELECT `date` FROM `wmeter` WHERE `device` = "'.$device.'" ORDER BY `date` DESC LIMIT 1;'); // cannot combine those two
 
-  $sql = 'SELECT `consumption`, `date`, '; // TODO: maybe add generation
-  $sql = $sql.'avg(`consDiff`) OVER(ORDER BY `date` DESC ROWS BETWEEN 5 PRECEDING AND CURRENT ROW ) as `movAveConsDiff`, ';
-  $sql = $sql.'avg(`dateDiff`) OVER(ORDER BY `date` DESC ROWS BETWEEN 5 PRECEDING AND CURRENT ROW ) as `movAveDateDiff` ';
-  $sql = $sql.'from `wmeter` WHERE `device` = "austr10" '; // TODO: device as variable
-  $sql = $sql.'ORDER BY `date` DESC LIMIT '.$QUERY_LIMIT.';';
+  $rowCnt = $resultCnt->fetch_assoc(); // returns one row only
+  $rowFreshest = $resultFreshest->fetch_assoc(); // returns 0 or 1 row
+  $totalCount = $rowCnt['total'];
+
+  printBeginOfPage_index($enableAutoReload);
+  $dateOldestString = '2020-01-01 08:00:00'; // some arbitrary date in the past
+  if ($totalCount > 0) {// this may be 0. Can't 
+    $dateNewest = date_create($rowFreshest['date']);    
+    if ($timeSelected < 25) {
+      $dateOldest = date_create($rowFreshest['date']);
+      $dateOldest->modify('-'.$timeSelected.' hours');
+      $dateOldestString = $dateOldest->format('Y-m-d H:i:s');
+    }
+
+    $QUERY_LIMIT = 5000; // TODO: check js-performance for a meaningful value (could omit it all together?)
+    $GRAPH_LIMIT = 3; // does not make sense to display a graph otherwise
+
+    $sql = 'SELECT `consumption`, `date`, '; // TODO: maybe add generation
+    $sql = $sql.'avg(`consDiff`) OVER(ORDER BY `date` DESC ROWS BETWEEN 5 PRECEDING AND CURRENT ROW ) as `movAveConsDiff`, ';
+    $sql = $sql.'avg(`dateDiff`) OVER(ORDER BY `date` DESC ROWS BETWEEN 5 PRECEDING AND CURRENT ROW ) as `movAveDateDiff` ';
+    $sql = $sql.'from `wmeter` WHERE `device` = "'.$device.'" AND `date` > "'.$dateOldestString.'" ';
+    $sql = $sql.'ORDER BY `date` DESC LIMIT '.$QUERY_LIMIT.';';    
   
-  $result = $dbConn->query($sql);
-  $queryCount = $result->num_rows; // this may be 0 ( = need to exclude from logic), or < graph-limit ( = display at least the newest) or >= graph-limit ( = all good)
+    $result = $dbConn->query($sql);
+    $rowNewest = $result->fetch_assoc(); // need to do this again to get the moving average stuff
+    $queryCount = $result->num_rows; // this may be < graph-limit ( = display at least the newest) or >= graph-limit ( = all good)
 
-  if ($queryCount > 0) { // have at least one. Can display the newest
-    $row_newest = $result->fetch_assoc();
-    $consumption_new = $row_newest['consumption'];
-    if ($row_newest['movAveDateDiff'] > 0) { // divide by 0 exception
-        $newestConsumption = round($row_newest['movAveConsDiff']*3600*1000 / $row_newest['movAveDateDiff']); // kWh compared to seconds
-    } else {$newestConsumption = 0.0; }
-    $date_new = date_create($row_newest['date']);
-
-    $dateString = 'um '.$date_new->format('Y-m-d H:i:s');
-    if (date('Y-m-d') === $date_new->format('Y-m-d')) { // same day
-      $dateString = 'heute um '.$date_new->format('H:i:s');  
+    if ($rowNewest['movAveDateDiff'] > 0) { // divide by 0 exception
+        $newestConsumption = round($rowNewest['movAveConsDiff']*3600*1000 / $rowNewest['movAveDateDiff']); // kWh compared to seconds
+    } else { $newestConsumption = 0.0; }
+    
+    $dateString = 'um '.$dateNewest->format('Y-m-d H:i:s');
+    if (date('Y-m-d') === $dateNewest->format('Y-m-d')) { // same day
+      $dateString = 'heute um '.$dateNewest->format('H:i:s');  
     }
     echo '<div class="row twelve columns"><hr>Verbrauch: <b>'.$newestConsumption.'W</b> '.$dateString.'<hr></div>';
 
@@ -76,7 +90,7 @@ if ($doSafe === 0) { // entry point of this site
       $val_y1_watt = '';
       
       while ($row = $result->fetch_assoc()) { // did already fetch the newest one. At least 2 remaining  
-        $consumption = $row['consumption'] - $consumption_new; // will be 0 or negative
+        $consumption = $row['consumption'] - $rowNewest['consumption']; // will be 0 or negative
         if ($row['movAveDateDiff'] > 0) { // divide by 0 exception
           $watt = max(round($row['movAveConsDiff']*3600*1000 / $row['movAveDateDiff']), 1.0); // max(val,1.0) because 0 in log will not be displayed correctly
         } else { $watt = 0; }
@@ -142,14 +156,13 @@ if ($doSafe === 0) { // entry point of this site
     echo '<div class="row twelve columns"> - noch keine Einträge - </div>';
   }
 
-  $resultCnt = $dbConn->query('SELECT COUNT(*) as `total` FROM `wmeter` WHERE `device` = "austr10";');
-  $rowTotal = $resultCnt->fetch_assoc(); // returns one row only
   $checkedText = '';
   if($enableAutoReload) {
     $checkedText = ' checked';
   }
   
   // TODO: add icons, change design
+  // TODO: depending on the number of entries, some ranges cannot be selected
   $submitTexts = array (
     '1' => array('1h','1 h',''),
     '6' => array('6h','6 h',''),
@@ -164,12 +177,12 @@ if ($doSafe === 0) { // entry point of this site
           }
           echo '</form></div>
           <div class="row">
-            <div class="six columns">Insgesamt '.$rowTotal['total'].' Einträge</div>
+            <div class="six columns">Insgesamt '.$totalCount.' Einträge</div>
             <div class="six columns"><div class="button"><a href="index.php?do=1">alle Einträge löschen</a></div></div>
-          </div>';
+          </div>'; // TODO: move the 'alles löschen' to another page, user page or whatever
 } elseif ($doSafe === 1) { // delete all entries, then go back to default page
   printBeginOfPage_index(FALSE);
-  $result = $dbConn->query('DELETE FROM `wmeter` WHERE 1'); // `device` = "home"); // MAYBE: want to delete only some things
+  $result = $dbConn->query('DELETE FROM `wmeter` WHERE `device` = "'.$device.'"');
   if ($result) {
     echo '<div class="row twelve columns">...alle Einträge gelöscht. <a href="index.php">zurück</a>...</div>';
   } else {
