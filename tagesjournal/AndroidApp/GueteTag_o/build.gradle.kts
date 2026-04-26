83218d3321ba64329cae974d68aeb21c03a6fb32
saliWd <57621907+saliWd@users.noreply.github.com>
2026-04-26 16:41:46 +0200

    focusing on one project now

--- tagesjournal/AndroidApp/GueteTag_o/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.androidx.room) apply false
-}
--- tagesjournal/Eintrag/app/proguard-rules.pro
+++
@@ -1,6 +0,0 @@
-# SQLCipher
--keep class net.sqlcipher.** { *; }
--keep class net.sqlcipher.database.** { *; }
-
-# Keep app model
--keep class ch.widmedia.Eintrag.Entry { *; }
